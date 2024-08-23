7448624faf96da709a7f487f464513e212e0cfff
Duong Intel <duongintel@Duongs-MacBook-Pro.local>
2024-08-23 13:54:38 -0600

    add screenshot

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,7 +1,8 @@
 import { test, expect } from '@playwright/test';
 
-test('start new browser context', async ({ page }) => {
+test('start new browser context', async ({ page }, testInfo) => {
     await page.goto('/')
     console.log('this is main TEST')
     //console.log('Hello ' + process.env.MY_ENV_VARIABLE)
+    await testInfo.attach('my-screenshot', { body: await page.screenshot(), contentType: 'image/png' });
 })
