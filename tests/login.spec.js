490acb4e72306b5a7ba1a9d0c7846fc023ff09f3
Duong Intel <duongintel@Duongs-MacBook-Pro.local>
2024-08-23 14:07:25 -0600

    read baseURL

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,8 +1,9 @@
 import { test, expect } from '@playwright/test';
 
-test('start new browser context', async ({ page }, testInfo) => {
+test('start new browser context', async ({ page, baseURL }, testInfo) => {
     await page.goto('/')
     console.log('this is main TEST')
+    console.log('running on:  ' + baseURL)
     //console.log('Hello ' + process.env.MY_ENV_VARIABLE)
     await testInfo.attach('my-screenshot', { body: await page.screenshot(), contentType: 'image/png' });
 })
