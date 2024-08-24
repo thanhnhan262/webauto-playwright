860406eff9015398a0d1a224d87a633f5c7b2217
Duong Intel <duongintel@Duongs-MacBook-Pro.local>
2024-08-24 10:18:23 -0600

    add missing digital library page

---
+++ pages/digitalLibraryPage.ts
@@ -0,0 +1,13 @@
+import { Page } from "playwright/test";
+import { ProfileNavigationComponent } from "./components/profileNavigatioComponent";
+
+export class DigitalLibraryPage {    
+
+    constructor(private page: Page){}
+    
+    //inherit
+    profileNavigation = new ProfileNavigationComponent(this.page)
+
+    //locators
+
+}
