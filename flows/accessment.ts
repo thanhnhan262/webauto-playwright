a20c2b1d8a241e88a829b628c62f5073a69d4576
Duong Intel <duongintel@Duongs-MacBook-Pro.local>
2024-08-23 19:24:37 -0600

    add components

--- flows/accessment.ts
+++ flows/accessment.ts
@@ -6,8 +6,8 @@ export class Accessment {
     constructor(private page: Page){}
     async login(user: string, pass: string) {
         const landingPage = new LandingPage(this.page)
-        await landingPage.profileIcon.click()
-        await landingPage.signInBtn.click()
+        await landingPage.profileNavigation.profileIcon.click()
+        await landingPage.profileNavigation.signInBtn.click()
         const loginPage = new LoginPage(this.page)
         await loginPage.userNameTbx.fill(user)
         await loginPage.passwordTbx.fill(pass)
--- pages/hompage.js
+++
@@ -1,9 +0,0 @@
-export class HomePage{
-
-    constructor(page){
-        this.page = page
-        this.logoutButton = this.page.locator("xpath=//i[contains(@class, 'icon-signout')]")
-        this.subHeader = this.page.locator("//*[@class='subheader']")
-    }
-    
-}
